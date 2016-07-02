8b3ec1380870b450bb3eda5e4489480f7b2083ac
macewindu <penn@horsecollaborative.com>
2016-07-02 11:17:20 -0400

    small changes to batch data structure -> no need to inherit from list

--- README.rst
+++ README.rst
@@ -1,6 +1,37 @@
-batbelt
+Batbelt
 --------
 
-Simply Do the Following::
+Example:
+```python
+import random
+import time
+random.seed(time.time())
+def print_stuff(index):
+	r = random.random() 
+	time.sleep(random)
+	print('Index:{}, Random:{}'.format(index, r))
+	return (index, r)
+```
 
-    >>> from batbelt import batch
+```python
+>>> from batbelt import Batch
+>>> batch = Batch(num_threads=4)
+>>> for i in range(8):
+		batch.append(print_stuff, args=[i])
+>>> batch.start()
+>>> batch.join()
+>>> for i in batch:
+		print(i)
+```
+
+Priority Queue Example:
+```
+>>> batch = Batch(num_threads=3, priority=True)
+>>> x = random.shuffle([i for i in range(12)])
+>>> print(x)
+>>> for i in x:
+		batch.append(print_stuff, args=[i], priority=i)
+>>> batch.start()
+>>> batch.join()
+>>> for i in batch
+		print(i)
--- batbelt/batch.py
+++ batbelt/batch.py
@@ -5,36 +5,24 @@ import queue
 from .job import Job
 from .worker import Worker
 
-class Batch(list):
+class Batch:
 	def __init__(self, num_threads, jobs=[], priority=False):
 		self.priority = priority
 		self.clear(jobs)
 		self._add_to_queue(jobs)
 		self._init_threads(num_threads)
 
-	def __setitem__(self, key, job):
-		self._check_type_job(job)
-		self.q.put(job)
-		list.__setitem__(self, inde, job)
-
 	def __getitem__(self, index):		# Returns the output of the function
-		return list.__getitem__(self, index).output
-
-	def __add__(self, jobs):
-		list.__add__(self, jobs)
-		self._add_to_queue(jobs)
+		return self.jobs[index].output
 
 	def __iter__(self):
 		for job in self.jobs:
 			yield job.output
 
-	def jobs(self, index):				# Returns the job that executed the function
-		return list.__getitem__(self, index)
-
 	def append(self, funct, args=[], priority=0):
 		job = Job(funct, args, priority)
 		self.q.put(job)
-		list.append(self, job)
+		self.jobs.append(job)
 
 	def start(self):
 		for thread in self.threads:
@@ -46,11 +34,11 @@ class Batch(list):
 			thread.join()
 
 	def clear(self, jobs = []):
-		list.__init__(self, jobs)
+		self.jobs = jobs
 		if not self.priority:
 			self.q = queue.Queue()
 		else:
-			self.q = queue.PriorityQUeue()
+			self.q = queue.PriorityQueue()
 
 	def _add_to_queue(self, jobs):
 		for job in jobs:
